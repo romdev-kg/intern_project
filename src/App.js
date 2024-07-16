9fde2197595308a6841b93367c3bf7c8566ae09d
adm1n-vibe <159103851+adm1n-vibe@users.noreply.github.com>
2024-07-16 16:07:23 +0600

    hui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './page/Home';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
     </div>
+    <Switch>
+      <Route path="/home">
+        <Home/>
+      </Route>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/page/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Banner.css';
+function Home() {
+  return (
+     
+    <div>
+        <img src="" alt="" />
+        <p>Home</p>
+        </div>
+    
+  )
+}
+
+export default Home
